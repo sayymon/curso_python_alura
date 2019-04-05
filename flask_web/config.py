f1192ab9526e9637c3ef619cb11751a88d3da06c
saymon <sayymon.silva@gmail.com>
2019-04-05 01:02:07 -0300

    Add files via upload

---
+++ flask_web/config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'saymon'
+
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "sa11y12mon92"
+MYSQL_DB = "jogoteca"
+MYSQL_PORT = 3306
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
